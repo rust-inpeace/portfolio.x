67135ba10b22f21d108c2f4b89f5b9664b36892e
rust-inpiece <somyaranjankhatua122@gmail.com>
2025-05-07 21:03:35 +0530

    deposit function added in fund manager

--- apps/contracts/src/FundManager.sol
+++ apps/contracts/src/FundManager.sol
@@ -1,6 +1,22 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+
 contract FundManager {
-    
+    mapping(address => mapping(address => uint256)) public userDeposit;
+
+    function deposit(address _token, uint256 _amount) external payable {
+        require(_amount > 0);
+        if (_token == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) {
+            require(msg.value == _amount);
+            userDeposit[msg.sender][_token] += msg.value;
+        } else {
+            require(msg.value == 0);
+            userDeposit[msg.sender][_token] += _amount;
+            require(
+                IERC20(_token).transferFrom(msg.sender, address(this), _amount)
+            );
+        }
+    }
 }
--- apps/contracts/src/interface/IRegistryTrader.sol
+++ apps/contracts/src/interface/IRegistryTrader.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.24;
 
 interface IRegistryTrader {
-    function registerTrader(address _trader, string memory _traderInfoUri) external payable;
+    function registerTrader(
+        address _trader,
+        string memory _traderInfoUri
+    ) external payable;
+
     function unregisterTrader(address _trader) external;
 }
