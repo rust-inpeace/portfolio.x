b7cbbd0fae85ca0de2cef1427c826ed7c438ed9d
rust-inpiece <somyaranjankhatua122@gmail.com>
2025-05-07 17:06:37 +0530

    create register ann unregister function for trader

--- apps/contracts/src/RegistryTrader.sol
+++ apps/contracts/src/RegistryTrader.sol
@@ -1,6 +1,42 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract RegistryTrader {
-    
+import {IRegistryTrader} from "@source/interface/IRegistryTrader.sol";
+
+contract RegistryTrader is IRegistryTrader {
+    uint256 public traderDepositAmount = 19 ether;
+
+    struct TraderStats {
+        address trader;
+        string traderInfoUri;
+        uint256 depositBalance;
+        bool varified;
+        bool canWithdraw;
+    }
+
+    mapping(address => TraderStats) public traderDeposit;
+
+    /// @inheritdoc IRegistryTrader
+    function registerTrader(
+        address _trader,
+        string memory _traderInfoUri
+    ) external payable {
+        require(msg.value >= traderDepositAmount);
+        traderDeposit[_trader] = TraderStats(
+            _trader,
+            _traderInfoUri,
+            msg.value,
+            false,
+            false
+        );
+    }
+
+    /// @inheritdoc IRegistryTrader
+    function unregisterTrader(address _trader) external {
+        require(traderDeposit[_trader].canWithdraw);
+        (bool success, ) = _trader.call{
+            value: traderDeposit[_trader].depositBalance
+        }("");
+        require(success);
+    }
 }
---
+++ apps/contracts/src/interface/IRegistryTrader.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IRegistryTrader {
+    function registerTrader(address _trader, string memory _traderInfoUri) external payable;
+    function unregisterTrader(address _trader) external;
+}
